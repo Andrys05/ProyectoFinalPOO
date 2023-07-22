42a86086ae6859fa6573002159c4b8c7c26b2e03
salda <salda@Andrys>
2023-07-22 18:11:33 -0400

    Primera implementación de clase citas

--- ProyectoFinalPoo/src/visual/RegCitas.java
+++ ProyectoFinalPoo/src/visual/RegCitas.java
@@ -1,12 +1,28 @@
 package visual;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-import javax.swing.JInternalFrame;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class RegCitas extends JInternalFrame {
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class RegCitas extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtNombre;
+	private JTextField txtCedula;
+	private JTextField txtDoctor;
+	private JTextField txtTipo;
+	private JTextField txtHora;
+	private JTextField txtFecha;
 
 	/**
 	 * Launch the application.
@@ -28,9 +44,104 @@ public class RegCitas extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public RegCitas() {
-		setTitle("Registrar Citas");
-		setBounds(100, 100, 818, 549);
-
+		setTitle("Registrar Cita");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 700, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("DATOS DEL PACIENTE:");
+		lblNewLabel.setFont(new Font("Arial Black", Font.BOLD, 16));
+		lblNewLabel.setBounds(15, 28, 242, 20);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		   
+		lblNombre.setFont(new Font("Arial Black", Font.PLAIN, 16));
+		lblNombre.setBounds(15, 89, 81, 20);
+		contentPane.add(lblNombre);
+		
+		JLabel lblCedula = new JLabel("C\u00E9dula:");
+		lblCedula.setFont(new Font("Arial Black", Font.PLAIN, 16));
+		lblCedula.setBounds(415, 89, 81, 20);
+		contentPane.add(lblCedula);
+		
+		JLabel lblDatosDeLa = new JLabel("DATOS DE LA CITA:");
+		lblDatosDeLa.setFont(new Font("Arial Black", Font.BOLD, 16));
+		lblDatosDeLa.setBounds(15, 149, 196, 20);
+		contentPane.add(lblDatosDeLa);
+		
+		JLabel lblDoctor = new JLabel("Doctor:");
+		lblDoctor.setFont(new Font("Arial Black", Font.PLAIN, 16));
+		lblDoctor.setBounds(15, 212, 81, 20);
+		contentPane.add(lblDoctor);
+		
+		JLabel lblTipo = new JLabel("Tipo:");
+		lblTipo.setFont(new Font("Arial Black", Font.PLAIN, 16));
+		lblTipo.setBounds(318, 212, 81, 20);
+		contentPane.add(lblTipo);
+		
+		JLabel lblFecha = new JLabel("Fecha:");
+		lblFecha.setFont(new Font("Arial Black", Font.PLAIN, 16));
+		lblFecha.setBounds(15, 285, 81, 20);
+		contentPane.add(lblFecha);
+		
+		JLabel lblHora_1 = new JLabel("Hora:");
+		lblHora_1.setFont(new Font("Arial Black", Font.PLAIN, 16));
+		lblHora_1.setBounds(318, 285, 81, 20);
+		contentPane.add(lblHora_1);
+		
+		txtNombre = new JTextField();
+		txtNombre.setBounds(96, 87, 304, 26);
+		contentPane.add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		txtCedula = new JTextField();
+		txtCedula.setColumns(10);
+		txtCedula.setBounds(486, 87, 146, 26);
+		contentPane.add(txtCedula);
+		
+		txtDoctor = new JTextField();
+		txtDoctor.setColumns(10);
+		txtDoctor.setBounds(86, 210, 214, 26);
+		contentPane.add(txtDoctor);
+		
+		txtTipo = new JTextField();
+		txtTipo.setColumns(10);
+		txtTipo.setBounds(368, 210, 175, 26);
+		contentPane.add(txtTipo);
+		
+		txtHora = new JTextField();
+		txtHora.setColumns(10);
+		txtHora.setBounds(377, 283, 146, 26);
+		contentPane.add(txtHora);
+		
+		txtFecha = new JTextField();
+		txtFecha.setColumns(10);
+		txtFecha.setBounds(81, 283, 146, 26);
+		contentPane.add(txtFecha);
+		
+		JButton btnRegistrar = new JButton("Registrar");
+		btnRegistrar.setBounds(408, 385, 115, 29);
+		contentPane.add(btnRegistrar);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(534, 385, 115, 29);
+		contentPane.add(btnCancelar);
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		//btnCancelar.setActionCommand("Cancel");
+		
 	}
 
 }
+
+
+
+
+		
